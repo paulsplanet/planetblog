fa3d83b2b0fa7e48f35200d2e56376fda7212433
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-10-22 18:02:18 -0700

    PostList Page - created

---
+++ src/Routes/PostList/PostListContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../Components/Header";
+
+
+const PostListContainer = () => {
+
+    return (
+        <>
+            <Header />
+            <div>
+                Post list here
+            </div>
+        </>
+    )
+};
+
+export default PostListContainer;
