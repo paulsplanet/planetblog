8b4ebbf53dccc2469a5619880cd3753b456fdae6
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-10-28 11:12:14 -0700

    New Post Button - created

--- src/Routes/PostList/PostListContainer.js
+++ src/Routes/PostList/PostListContainer.js
@@ -1,16 +1,54 @@
 import React from "react";
 import Header from "../../Components/Header";
+import styled from "styled-components";
+import Button from "../../Components/utils/Button";
+import { useSelector } from "react-redux";
 
+const PostListBlock = styled.div`
+    margin-top: 3rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    width: 1024px;
+    margin: 0 auto;
 
+    @media (max-width: 1024px) {
+        width: 768px;
+    }
+    @media (max-width: 768px) {
+        width: 100%;
+    }
+`;
+
+const WritePostButtonWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin: 3rem 0;
+`;
+
+const PostButton = styled(Button)`
+    font-size: 1.125rem;
+    width: 170px;
+    padding: 0.45rem 0;
+    display: flex;
+    justify-content: center;
+`;
 
 const PostListContainer = () => {
+    const user = useSelector(state => state.auth.user.id)
 
     return (
         <>
             <Header />
-            <div>
-                Post list here
-            </div>
+            <PostListBlock>
+                <WritePostButtonWrapper>
+                    { user && (
+                        <PostButton orange to='/write'>New Post</PostButton>
+                    )}
+                </WritePostButtonWrapper>
+                <div>
+                    present posts here
+                </div>
+            </PostListBlock>
         </>
     )
 };
